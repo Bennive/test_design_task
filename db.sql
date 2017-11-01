80fa2b645f519be3b9632a2ca978274c68060872
Ivan Trechyokas <i.trechyokas@semrush.com>
2017-11-01 11:59:19 +0300

    [New idea] tadam

--- db.sql
+++ db.sql
@@ -10,13 +10,13 @@ SET time_zone = "+00:00";
 DROP TABLE IF EXISTS 'user';
 CREATE TABLE IF NOT EXISTS 'user' (
   'registration_date' datetime NOT NULL DEFAULT '1979-01-01 00:00:00',
-  'password' varchar(32) NOT NULL,
-  'email' varchar(32) NOT NULL,
-  'name' varchar(32) DEFAULT NULL,
-  'activated' tinyint(1) NOT NULL DEFAULT '0',
+  'password' varchar(20) NOT NULL,
+  'email' varchar(24),
+  'name' varchar(20) DEFAULT NULL,
+  'activated' tinyint(1) DEFAULT '0',
   'admin' tinyint(1) NOT NULL DEFAULT '0',
-  'phone' char(11) DEFAULT NULL,
-  'country' varchar(10) DEFAULT NULL,
-  'domain' varchar(20) DEFAULT NULL,
-  'disabled' tinyint(1) NOT NULL DEFAULT '0'
+  'phone' char(10) DEFAULT NULL,
+  'country' varchar(3) DEFAULT NULL,
+  'domain' varchar(16) DEFAULT NULL,
+  'disabled' tinyint(1) DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
